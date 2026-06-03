8fb5d06e81e6f87601f0d0d332a54f47699254ce
salvabasiuk <salvador.costa@escola.pr.gov.br>
2026-06-03 08:26:57 -0300

    aula3

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ console.log(botoes);
 for (let i = 0; i < botoes.length; i++) {
   console.log(i);
 }
-botoes[i].onclick = function () {};
+botoes[i].onclick = function () { };
 botoes[i].onclick = function () {
   botoes[i].classList.add("ativo");
 };
@@ -14,7 +14,7 @@ for (let i = 0; i < botoes.length; i++) {
 }
 for (let j = 0; j < botoes.length; j++) {
   botoes[j].classList.remove("ativo");
-}const botoes = document.querySelectorAll(".botao");
+} const botoes = document.querySelectorAll(".botao");
 
 for (let i = 0; i < botoes.length; i++) {
   botoes[i].onclick = function () {
@@ -24,4 +24,8 @@ for (let i = 0; i < botoes.length; i++) {
 
     botoes[i].classList.add("ativo");
   };
-}
+} const contadores = document.querySelectorAll(".contador");
+const tempoObjetivo1 = new Date("2023-10-05T00:00:00");
+let tempoAtual = new Date();
+
+contadores[0].textContent = tempoObjetivo1 - tempoAtual;
